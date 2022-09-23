efea8c584fa1f333de4a894ed38a6c9d701fae77
Louys <louysdev@gmail.com>
2022-09-23 11:53:32 -0400

    QueryAll y QuerybyId

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,15 @@
 const heading = document.querySelector("a")
 console.log(heading)
 
-
-
 // querySelectorAll
+const enlaces = document.querySelectorAll(".navegacion a")
+console.log(enlaces[0])
+enlaces[0].textContent = "Nuevo texto para enlace"
+enlaces[0].href = "www.google.com"
+enlaces[0].classList.add("nueva")
+enlaces[0].classList.remove("navegacion__enlace")
+
 
 // getElementById
+
+const heading2 = document.getElementById("#heading")
