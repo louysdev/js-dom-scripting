fe1c77049277e88723e5c03bf0e683958f561236
Louys <louysdev@gmail.com>
2022-09-23 12:04:42 -0400

    Create element

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,28 @@
 // querySelector
 const heading = document.querySelector("a")
-console.log(heading)
+// console.log(heading)
 
 // querySelectorAll
 const enlaces = document.querySelectorAll(".navegacion a")
-console.log(enlaces[0])
-enlaces[0].textContent = "Nuevo texto para enlace"
-enlaces[0].href = "www.google.com"
-enlaces[0].classList.add("nueva")
-enlaces[0].classList.remove("navegacion__enlace")
+// console.log(enlaces[0])
+// enlaces[0].textContent = "Nuevo texto para enlace"
+// enlaces[0].href = "www.google.com"
+// enlaces[0].classList.add("nueva")
+// enlaces[0].classList.remove("navegacion__enlace")
 
 
 // getElementById
 
 const heading2 = document.getElementById("#heading")
+
+// Generar un nuevo enlace
+const nuevoEnlance = document.createElement("a")
+
+nuevoEnlance.href = "nuevo-enlace.html"
+nuevoEnlance.textContent = "Nuevo enlace"
+nuevoEnlance.classList.add("navegacion__enlace")
+
+const navegacion = document.querySelector(".navegacion")
+navegacion.appendChild(nuevoEnlance)
+
+console.log(nuevoEnlance)
