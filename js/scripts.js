3045eaeadefc48baf80ab32688d33b3ed75968af
Louys <louysdev@gmail.com>
2022-09-23 23:42:36 -0400

    Eventos de teclado

--- js/scripts.js
+++ js/scripts.js
@@ -25,30 +25,30 @@ nuevoEnlance.classList.add("navegacion__enlace")
 const navegacion = document.querySelector(".navegacion")
 navegacion.appendChild(nuevoEnlance)
 
-console.log(nuevoEnlance)
+// console.log(nuevoEnlance)
 
 
 // Eventos
 
-console.log(1)
+// console.log(1)
 
 window.addEventListener("load", function() {
-    console.log(2)
+    // console.log(2)
 })
 
 window.onload = function(){
-    console.log(3)
+    // console.log(3)
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-    console.log(4)
+    // console.log(4)
 })
 
 window.onscroll = function() {
-    console.log("Moviendo")
+    // console.log("Moviendo")
 }
 
-console.log(15)
+// console.log(15)
 
 
 // Seleccionar elementos
@@ -60,3 +60,24 @@ btnEnviar.addEventListener("click", function(evento) {
     console.log("Enviando formulario")
 })
 
+// Eventos de teclado
+
+const datos = {
+    nombre: "",
+    email: "",
+    mensaje: ""
+}
+
+const nombre = document.querySelector("#nombre")
+const email = document.querySelector("#email")
+const mensaje = document.querySelector("#mensaje")
+
+nombre.addEventListener("input", leerTexto)
+email.addEventListener("input", leerTexto)
+mensaje.addEventListener("input", leerTexto)
+
+function leerTexto(evento) {
+    console.log(evento.target.value)
+    datos[evento.target.id] = evento.target.value
+    console.log(datos)
+}
